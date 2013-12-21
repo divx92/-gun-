4f19a542327cac1fc577b1ca9db8eba601df3cc0
Filip <pssmaster@interia.eu>
2013-12-21 22:03:03 +0100

    Create list.php

---
+++ forum/game/modules_public/tickets/list.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * GUN Application
+ *
+ * @author     Filip Sosnowski <filipsosnowski38@interia.eu>
+ * @copyright  2013 Filip Sosnowski
+ */
+ 
+class public_rp_tickets_list extends ipsCommand
+{
+	public function doExecute( ipsRegistry $registry )
+	{
+		require_once( IPSLib::getAppDir( 'rp' ) . '/sources/tickets.php' );
+		$this->registry->setClass( 'tickets', new tickets( $registry ) );
+		$tickets = $this->registry->getClass( 'tickets' );
+		
+		$tickets = $tickets->fetchTickets( $this->memberData['member_id'] );
+		
+		$this->registry->output->setTitle( 'ZgĹ‚oszenia' );
+		$this->registry->output->addNavigation( 'ZgĹ‚oszenia', 'app=rp&module=tickets' );
+		$this->registry->output->addContent( $this->registry->output->getTemplate( 'rp' )->tickets_list( $tickets ) );
+		$this->registry->output->sendOutput();
+	}
+}
