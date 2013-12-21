3c5516bcce0fbe75ca9c39ed153111bdb8e2ba5d
Filip <pssmaster@interia.eu>
2013-12-21 22:14:46 +0100

    Update list.php

--- forum/game/modules_public/tickets/list.php
+++ forum/game/modules_public/tickets/list.php
@@ -7,11 +7,11 @@
  * @copyright  2013 Filip Sosnowski
  */
  
-class public_rp_tickets_list extends ipsCommand
+class public_game_tickets_list extends ipsCommand
 {
 	public function doExecute( ipsRegistry $registry )
 	{
-		require_once( IPSLib::getAppDir( 'rp' ) . '/sources/tickets.php' );
+		require_once( IPSLib::getAppDir( 'game' ) . '/sources/tickets.php' );
 		$this->registry->setClass( 'tickets', new tickets( $registry ) );
 		$tickets = $this->registry->getClass( 'tickets' );
 		
@@ -19,7 +19,7 @@ class public_rp_tickets_list extends ipsCommand
 		
 		$this->registry->output->setTitle( 'Zgłoszenia' );
 		$this->registry->output->addNavigation( 'Zgłoszenia', 'app=rp&module=tickets' );
-		$this->registry->output->addContent( $this->registry->output->getTemplate( 'rp' )->tickets_list( $tickets ) );
+		$this->registry->output->addContent( $this->registry->output->getTemplate( 'game' )->tickets_list( $tickets ) );
 		$this->registry->output->sendOutput();
 	}
 }
