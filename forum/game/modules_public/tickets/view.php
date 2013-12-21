ec0e7ed96340959185715075f56caa6fa100a2c4
Filip <pssmaster@interia.eu>
2013-12-21 22:15:41 +0100

    Update view.php

--- forum/game/modules_public/tickets/view.php
+++ forum/game/modules_public/tickets/view.php
@@ -7,11 +7,11 @@
  * @copyright  2013 Filip Sosnowski
  */
 
-class public_rp_tickets_view extends ipsCommand
+class public_game_tickets_view extends ipsCommand
 {
 	public function doExecute( ipsRegistry $registry )
 	{
-		require_once( IPSLib::getAppDir( 'rp' ) . '/sources/tickets.php' );
+		require_once( IPSLib::getAppDir( 'game' ) . '/sources/tickets.php' );
 		$this->registry->setClass( 'tickets', new tickets( $registry ) );
 		$tickets = $this->registry->getClass( 'tickets' );
 		
@@ -47,7 +47,7 @@ class public_rp_tickets_view extends ipsCommand
 		$this->registry->output->setTitle( 'Zgłoszenia' );
 		$this->registry->output->addNavigation( 'Zgłoszenia', 'app=rp&module=tickets' );
 		$this->registry->output->addNavigation( ''. $ticket['ticket_title'] .' (UID: '. $ticket['ticket_uid'] .')', 'app=rp&module=tickets&section=view&ticket='. $ticket['ticket_uid'] .'' );
-		$this->registry->output->addContent( $this->registry->output->getTemplate( 'rp' )->tickets_view( $ticket, $answers, $html ) );
+		$this->registry->output->addContent( $this->registry->output->getTemplate( 'game' )->tickets_view( $ticket, $answers, $html ) );
 		$this->registry->output->sendOutput();
 	}
 }
