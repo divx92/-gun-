9ecb62fb3e9a84dd16ceec2198f21031fc95fd8b
Filip <pssmaster@interia.eu>
2013-12-21 21:59:02 +0100

    Create coreVariables.php

---
+++ forum/game/extensions/coreVariables.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * GUN Application
+ *
+ * @author     Filip Sosnowski <filipsosnowski38@interia.eu>
+ * @copyright  2013 Filip Sosnowski
+ */
+
+if( ! isset( $_REQUEST['module'] ) )
+{
+	$_RESET['app']	= 'rp';
+	$_RESET['module']	= 'tickets';
+	$_RESET['section']	= 'list';
+}
